1b1a5d8dbd0610a7c6ba46108f22ef84ebda942c
Adeola Ojo <ojoadeolagabriel@gmail.com>
2016-05-21 20:59:20 +0100

    clean route

--- nerve.core/synapse/dataobjects/Route.cs
+++ nerve.core/synapse/dataobjects/Route.cs
@@ -9,9 +9,7 @@ namespace nerve.core.synapse.dataobjects
     public class Route
     {
         public string Description { get; set; }
-
         public PackageDescriptor PackageDescriptor { get; set; }
-
         public RouteStep CurrentRouteStep { get; set; }
         public string RouteId { get; set; }
     }
